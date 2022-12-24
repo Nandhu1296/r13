5c0da8375bc91189f7adca2cfa064e0214b46a41
Priyanka <nandhu1296@ymail.om>
2022-12-24 00:21:14 -0600

    commit after long

--- r13/ViewController.swift
+++ r13/ViewController.swift
@@ -1,19 +1,13 @@
-//
-//  ViewController.swift
-//  r13
-//
-//  Created by PRIYANKA SANKAR on 12/22/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class WayTBC: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Tab Bar Customisation
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
